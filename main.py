28913b6284e090f1c54d685d2145a5d4d44d2039
IntegraMaker <integramaker.ss@ifpb.edu.br>
2026-06-12 14:38:57 -0300

    Atualização da hora

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from datetime import datetime
 
 janela = tk.Tk()
 
@@ -12,17 +13,35 @@ janela.attributes("-fullscreen", True)
 
 janela.config(cursor="none")
 
+# Ajustando o mostrador da hora
+
+hora = tk.Label(janela,text=datetime.now().strftime("%H:%M:%S"),fg="white", bg="black",
+                  font=("Arial",30))
+hora.pack()
+
 
 rotulo = tk.Label(janela,text="Sirene interativa",fg="white", bg="black",
                   font=("Arial",40))
 rotulo.pack(expand=True)
 
 
+#Funções
+
+def atualizar_hora():
+    
+    hora.config(text=datetime.now().strftime("%H:%M:%S"))
+    
+    janela.after(1000, atualizar_hora)
+
+
+
 def fechar_janela(event):
     janela.destroy()
     
-    
+
 janela.bind("<Escape>", fechar_janela)
 
+
+atualizar_hora()
 
 janela.mainloop()
